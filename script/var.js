24e07fab51a3ca7ad1c640154f3eb07c5208cfcf
mratomicbomb <36752864+mratomicbomb@users.noreply.github.com>
2018-02-24 22:17:00 -0500

    Update var.js

--- script/var.js
+++ script/var.js
@@ -6,7 +6,8 @@ var clientid = "be212a58528168962a39c64052c1d88e";
 var redirecturi = "http://localhost:8002/callback.html";
 var locations = [
   "2361037",
-  "784302"
+  "2355123",
+  "2452741"
 ];
 var images = [
   "1412446496031.jpg",
